ddf6f2d9771ab5a6d5379e7bc671dd9c22475478
alswlfl29 <budlalswl1129@gmail.com>
2024-04-03 10:08:36 +0900

    실습과제3 풀이

--- Pr03VM/src/main/java/com/study/Pr03VM/MainController.java
+++ Pr03VM/src/main/java/com/study/Pr03VM/MainController.java
@@ -1,14 +1,11 @@
 package com.study.Pr03VM;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -27,38 +24,52 @@ public class MainController {
         return "addItem";
     }
 
+    // Form의 Input을 받는 방법
+    // 1. @RequestParam 단일 매개변수
+    // 2. @ModelAttribute 객체 매핑(바인딩)
     @PostMapping("/add")
-    public String productAdd(@RequestParam String pName, @RequestParam int pPrice, @RequestParam LocalDate pExpDate){
-        Product product = new Product();
-        product.setName(pName);
-        product.setPrice(pPrice);
-        product.setLimit_date(pExpDate);
+    @ResponseBody
+    public String productAdd(
+            // null 을 허용한다.
+            // @RequestParam(required = false, defaultValue = "기본값") String pName,
+            @RequestParam String pName,
+            @RequestParam int pPrice,
+            @RequestParam LocalDate pExpDate){
+        Product product = Product.builder()
+                .name(pName)
+                .price(pPrice)
+                .limit_date(pExpDate)
+                .build();
 
         productList.add(product);
-        return "redirect:/";
+        // return "redirect:/";
+        // 웹브라우저에게 응답을 주면서, 리다이렉트할 경로를 전달하면, 웹브라우저는 받자마자 이 경로로 다시 요청함.
+        return "<script>alert('추가되었습니다.');location.href='/';</script>";
     }
 
     @GetMapping("/update")
     public String productUpdate(@RequestParam int index, Model model){
-        model.addAttribute("product",productList.get(index));
+        Product product = productList.get(index);
+        model.addAttribute("product",product);
         model.addAttribute("index",index);
         return "updateItem";
     }
 
     @PostMapping("/update")
-    public String updateAction(@RequestParam int index, @RequestParam String pName, @RequestParam int pPrice, @RequestParam LocalDate pExpDate, Model model){
-        Product product = new Product();
+    @ResponseBody
+    public String updateAction(@RequestParam int index, @RequestParam String pName, @RequestParam int pPrice, @RequestParam LocalDate pExpDate){
+        Product product = productList.get(index);
         product.setName(pName);
         product.setPrice(pPrice);
         product.setLimit_date(pExpDate);
 
         productList.set(index, product);
-        return "redirect:/";
+        return "<script>alert('수정되었습니다.');location.href='/';</script>";
     }
 
     @GetMapping("/delete")
     @ResponseBody
-    public String delete(@RequestParam int index, Model model){
+    public String delete(@RequestParam int index){
         productList.remove(index);
         return "<script>alert('삭제되었습니다.');location.href='/';</script>";
     }
